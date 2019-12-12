35da5b70817d404b5a318d9a367252ddb9954a94
staffgha <ghall@roundhayschool.com>
2019-12-12 14:59:58 +0000

    ADDED TITLE SCREEN

--- hello.py
+++ hello.py
@@ -9,8 +9,8 @@ import states
 pygame.init()
 screen = pygame.display.set_mode(SIZE)
 clock = pygame.time.Clock()
-current_state = states.Game()
-
+#current_state = states.Game()
+current_state = states.TitleScreen()
 
 #Game loop
 while current_state.running:
@@ -18,7 +18,9 @@ while current_state.running:
     if current_state.next_state:
         current_state = current_state.next_state
     events = pygame.event.get()
-    current_state.handle_events(events)
+    for event in events:
+
+        current_state.handle_event(event)
     #game logic
     current_state.update()
     #drawing
--- states.py
+++ states.py
@@ -2,9 +2,24 @@ from Constants import *
 import pygame
 import Sprites
 
-
-class Game():
+class States():
     def __init__(self):
+        super().__init__()
+        self.running = True
+        self.next_state = None
+    def handle_event(self, event):
+        pass
+
+    def update(self):
+        pass
+
+    def draw(self, screen):
+        pass
+
+
+class Game(States):
+    def __init__(self):
+        super().__init__()
         # initalise sprites
         self.sprites = pygame.sprite.Group()
         self.left_paddle = Sprites.Paddle(LEFT_PADDLE_X, STARTING_PADDLE_Y)
@@ -14,33 +29,31 @@ class Game():
         self.paddles = [self.left_paddle, self.right_paddle]
         self.ball = Sprites.Ball(STARTING_BALL_X, STARTING_BALL_Y, self.paddles)
         self.sprites.add(self.ball)
-        self.running = True
         self.next_state = None
 
-    def handle_events(self, events):#
-        for event in events:
-            if event.type == pygame.QUIT:
-                self.running = False
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_a:
-                    self.left_paddle.up()
-                elif event.key == pygame.K_z:
-                    self.left_paddle.down()
-                elif event.key == pygame.K_UP:
-                    self.right_paddle.up()
-                elif event.key == pygame.K_DOWN:
-                    self.right_paddle.down()
-                elif event.key == pygame.K_q:
-                    self.next_state = Menu()
-            elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_a:
-                    self.left_paddle.stopup()
-                elif event.key == pygame.K_z:
-                    self.left_paddle.stopdown()
-                elif event.key == pygame.K_UP:
-                    self.right_paddle.stopup()
-                elif event.key == pygame.K_DOWN:
-                    self.right_paddle.stopdown()
+    def handle_event(self, event):#
+        if event.type == pygame.QUIT:
+            self.running = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_a:
+                self.left_paddle.up()
+            elif event.key == pygame.K_z:
+                self.left_paddle.down()
+            elif event.key == pygame.K_UP:
+                self.right_paddle.up()
+            elif event.key == pygame.K_DOWN:
+                self.right_paddle.down()
+            elif event.key == pygame.K_q:
+                self.next_state = Menu()
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_a:
+                self.left_paddle.stopup()
+            elif event.key == pygame.K_z:
+                self.left_paddle.stopdown()
+            elif event.key == pygame.K_UP:
+                self.right_paddle.stopup()
+            elif event.key == pygame.K_DOWN:
+                self.right_paddle.stopdown()
 
 
     def update(self):
@@ -51,18 +64,38 @@ class Game():
         self.sprites.draw(screen)
 
 
-class Menu():
+class Menu(States):
     def __init__(self):
-        self.running = True
-        self.next_state = None
+        super().__init__()
 
-    def handle_events(self, events):
-        for event in events:
-            if event.type == pygame.QUIT:
-                self.running = False
+
+    def handle_event(self, event):
+        if event.type == pygame.QUIT:
+            self.running = False
 
     def update(self):
         pass
 
     def draw(self, screen):
         screen.fill(BLUE)
+
+class TitleScreen(States):
+    def __init__(self):
+        super().__init__()
+        self.background = pygame.image.load("images/background.png").convert()
+        self.timer = 0
+
+    def handle_event(self, event):
+        if event.type == pygame.QUIT:
+            self.running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            self.next_state = Game()
+
+    def update(self):
+        self.timer += 1
+        if self.timer > 500:
+            self.next_state = Game()
+
+    def draw(self, screen):
+        screen.fill(GREEN)
+        screen.blit(self.background, (0,0))
